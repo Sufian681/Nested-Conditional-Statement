48772c52d00f8d4bdac96d0270776171d9f84a65
Sufi <sufiansonic@gmail.com>
2025-04-26 12:34:18 +0900

    shq

---
+++ Checkage.py
@@ -0,0 +1,14 @@
+
+age = int (input ( "enter your age : "))
+
+if age < 10 :
+    print ( "you are too young for this class" )
+    
+elif age > 20 :
+    print ( "you are too old for this class" )
+    
+else :
+    print ( "you can do this class")
+
+    
+            
